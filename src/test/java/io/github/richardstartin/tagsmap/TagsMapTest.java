ac428e04e0b6849a164da3d2bc88e97305ea95b3
Richard Startin <richard@openkappa.co.uk>
2020-06-06 19:28:15 +0200

    add some noddy testing

--- src/test/java/io/github/richardstartin/tagsmap/TagsMapTest.java
+++ src/test/java/io/github/richardstartin/tagsmap/TagsMapTest.java
@@ -2,6 +2,9 @@ package io.github.richardstartin.tagsmap;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TagsMapTest {
@@ -20,6 +23,46 @@ class TagsMapTest {
     map.makeImmutable();
     assertEquals("x1", map.getExclusive("x1"));
     assertEquals(10, map.getExclusive("x2"));
+  }
+
+  @Test
+  public void visibilityTest() throws InterruptedException {
+    AtomicLong counter = new AtomicLong();
+    AtomicLong puts = new AtomicLong();
+    TagsMap<Long> map = TagsMap.create(String.class, "x1", "x2");
+    CountDownLatch latch = new CountDownLatch(1);
+    Thread writer = new Thread(() -> {
+      map.put("x1", counter.get());
+      long count = 0;
+      latch.countDown();
+      for (int i = 0; i < 100000; ++i) {
+        count = counter.get();
+        map.put("x1", count);
+      }
+      puts.set(count);
+    });
+
+
+    Thread reader = new Thread(() -> {
+      try {
+        latch.await();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        return;
+      }
+      for (int i = 0; i < 100000; ++i) {
+        Long value = map.get("x1");
+        assertTrue(value <= counter.getAndIncrement());
+        assertTrue(value >= puts.get());
+      }
+    });
+    reader.start();
+    writer.start();
+    writer.join();
+    assertEquals(puts.get(), map.get("x1"));
+    reader.join();
+    map.makeImmutable();
+    assertEquals(puts.get(), map.getExclusive("x1"));
   }
 
 }
